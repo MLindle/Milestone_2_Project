4ffc92e08c9b3f6e49db065671e3b79f1f1c874e
Myzerberries <52772439+Myzerberries@users.noreply.github.com>
2025-10-05 16:32:45 -0500

    Updated script to write resume analyitcs to DDB, added environment variables in workflow to be used by script to pass deployment values to DDB, added the creation of the env key before script executes.

--- process_resume.py
+++ process_resume.py
@@ -1,77 +1,116 @@
 #!/usr/bin/env python3
 
-import boto3, json
+# import boto3, json, os
+# from datetime import datetime, timezone
 
-s3 = boto3.client('s3')
-bedrock = boto3.client('bedrock-runtime', region_name='us-east-1')
+# s3 = boto3.client('s3')
+# bedrock = boto3.client('bedrock-runtime', region_name='us-east-1')
+# dynamodb = boto3.client('dynamodb')
 
-resume_contents = open("resume_template.md", "r").read()
+# resume_contents = open("resume_template.md", "r").read()
 
-prompt_template = open("prompt_template.txt", "r").read()
+# prompt_template = open("prompt_template.txt", "r").read()
 
-resume_payload = {
-    "anthropic_version": "bedrock-2023-05-31",
-    "messages": [{
-        "role": "user", 
-        "content": [{
-            "type": "text",
-            "text": (
-                "This resume is designed for cloud engineering/DevOps. Please convert"
-                "it and return the results in valid HTML, in JSON format. Return only"
-                " the HTML, no JSON wrapper, no code fences. Please only"
-                "include the output for the HTML value starting with <!DOCTYPE html>"
-                "to make this easier to use:"
-                f"{resume_contents}"
-            )
-        }]
-    }],
-    "max_tokens": 4096,
-    "temperature": 0
-}
+# resume_payload = {
+#     "anthropic_version": "bedrock-2023-05-31",
+#     "messages": [{
+#         "role": "user", 
+#         "content": [{
+#             "type": "text",
+#             "text": (
+#                 "This resume is designed for cloud engineering/DevOps. Please convert"
+#                 "it and return the results in valid HTML, in JSON format. Return only"
+#                 " the HTML, no JSON wrapper, no code fences. Please only"
+#                 "include the output for the HTML value starting with <!DOCTYPE html>"
+#                 "to make this easier to use:"
+#                 f"{resume_contents}"
+#             )
+#         }]
+#     }],
+#     "max_tokens": 4096,
+#     "temperature": 0
+# }
 
-response = bedrock.invoke_model(
-    modelId="anthropic.claude-3-sonnet-20240229-v1:0",
-    body=json.dumps(resume_payload),
-    contentType="application/json",
-    accept="application/json"
-)
+# response = bedrock.invoke_model(
+#     modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+#     body=json.dumps(resume_payload),
+#     contentType="application/json",
+#     accept="application/json"
+# )
 
-bedrock_result_resume= json.loads(response["body"].read())
+# bedrock_result_resume= json.loads(response["body"].read())
 
-resume_json_string = json.dumps(bedrock_result_resume)
+# resume_json_string = json.dumps(bedrock_result_resume)
 
-resume_analytics_payload = {
-    "anthropic_version": "bedrock-2023-05-31",
-    "messages": [{
-        "role": "user",
-        "content": [{
-            "type": "text",
-            "text": (
-                f"Can you process this JSON{bedrock_result_resume}"
-                "that states an ATS score, word count,"
-                "keywords, readability, and missing sections for this resume"
-                ", outputting as JSON and using the following format: \n"
-                f"{prompt_template}""Please only provide the output in the format"
-                "requested with no extra explanation so this can be easily used as"
-                "data to be sent to DynamoDB"
-            )
-        }]
-    }],
-    "max_tokens": 4096,
-    "temperature": 0
-}
+# resume_analytics_payload = {
+#     "anthropic_version": "bedrock-2023-05-31",
+#     "messages": [{
+#         "role": "user",
+#         "content": [{
+#             "type": "text",
+#             "text": (
+#                 f"Can you process this JSON{bedrock_result_resume}"
+#                 "that states an ATS score, word count,"
+#                 "keywords, readability, and missing sections for this resume"
+#                 ", outputting as JSON and using the following format: \n"
+#                 f"{prompt_template}""Please only provide the output in the format"
+#                 "requested with no extra explanation so this can be easily used as"
+#                 "data to be sent to DynamoDB"
+#             )
+#         }]
+#     }],
+#     "max_tokens": 4096,
+#     "temperature": 0
+# }
 
-analytics_response = bedrock.invoke_model(
-    modelId="anthropic.claude-3-sonnet-20240229-v1:0",
-    body=json.dumps(resume_analytics_payload),
-    contentType="application/json",
-    accept="application/json"
-)
+# analytics_response = bedrock.invoke_model(
+#     modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+#     body=json.dumps(resume_analytics_payload),
+#     contentType="application/json",
+#     accept="application/json"
+# )
 
-analytics_result = json.loads(analytics_response["body"].read())
+# analytics_result = json.loads(analytics_response["body"].read())
 
-br_results = bedrock_result_resume["content"][0]["text"]
+# br_results = bedrock_result_resume["content"][0]["text"]
 
-with open("converted_resume.html", "w") as f:
-    f.write(br_results)
+# analytics_results = json.loads(analytics_result["content"][0]["text"])
+
+# for values in analytics_results:
+#     #print (values, analytics_results.get(values))
+#     if values == 'ats_score':
+#         ats_score = analytics_results.get(values)
+#     elif values == 'word_count':
+#         word_count = analytics_results.get(values)
+#     elif values == 'keywords':
+#         keywords = analytics_results.get(values)
+#     elif values == 'readability_score':
+#         readability_score = analytics_results.get(values)
+#     elif values == 'missing_sections':
+#         missing_sections = analytics_results.get(values)
+
+#print (ats_score, word_count, keywords, readability_score, missing_sections)
+
+# current_utc_datetime = datetime.now(timezone.utc).isoformat()
+# print (current_utc_datetime)
+
+# response = dynamodb.put_item(
+#     TableName='Milestone-2-project-resume-analytics-table',
+#     Item={
+#         'timestamp': {'S': current_utc_datetime},
+#         'ats_score': {'N': str(ats_score)},
+#         'word_count': {'N': str(word_count)},
+#         'keywords': {'SS': keywords}
+#         }
+    
+# )
+
+# github_commit_sha = os.getenv("GITHUB_SHA", "")
+# deployment_status = os.getenv("DEPLOY_STATUS", "unknown")
+# deployment_environment = os.getenv("DEPLOYMENT_ENV", "")
+# model_used = "anthropic.claude-3-sonnet"
+# s3_url = os.getenv("S3_URL", "")
+
+# with open("converted_resume.html", "w") as f:
+#     f.write(br_results)
 
